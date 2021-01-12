aa8e82cf2b9e2d4ea6eaa1bbb8f5b2d1e6580da2
Faridjadda <faridjadda750@gmail.com>
2021-01-12 18:13:51 +0100

    add new home.dart view

--- musicweek/lib/views/home.dart
+++ musicweek/lib/views/home.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:flutter/rendering.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(appBar: AppBar(
-          title: Text("Flutter GridView Demo"),
-        ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: Colors.grey[850],
+            appBar: AppBar(
+              leading: Icon(Icons.home),
+              actions: [Icon(Icons.more_vert)],
+              title: Text("Flutter GridView Demo"),
+              backgroundColor: Colors.grey[850],
+            ),
             body: GridView.count(
-                crossAxisCount: 4,
+                crossAxisCount: 3,
                 crossAxisSpacing: 6.0,
-                mainAxisSpacing: 10.0,
+                mainAxisSpacing: 6.0,
                 children: List.generate(choices.length, (index) {
                   return Center(
                     child: SelectCard(choice: choices[index]),
                   );
-                }
-                )
-            )
-        )
-    );
+                }))));
   }
 }
 
@@ -32,14 +33,18 @@ class Choice {
 }
 
 const List<Choice> choices = const <Choice>[
-  const Choice(title: 'Conversion taille de fichiers informatique', icon: Icons.broken_image),
+  const Choice(
+      title: 'Conversion taille de fichiers informatique',
+      icon: Icons.broken_image),
   const Choice(title: 'Connaître mon âge', icon: Icons.person),
   const Choice(title: 'Calculer des promotions', icon: Icons.calculate),
   const Choice(title: 'Calculer un temps écoulé', icon: Icons.timelapse),
   const Choice(title: 'Convertir une distance', icon: Icons.edit_road),
   const Choice(title: 'Convertir des aires', icon: Icons.ac_unit),
   const Choice(title: 'Convertir des températures', icon: Icons.ac_unit_sharp),
-  const Choice(title: 'Convertir décimal en chiffre romain', icon: Icons.calculate_outlined),
+  const Choice(
+      title: 'Convertir décimal en chiffre romain',
+      icon: Icons.calculate_outlined),
   const Choice(title: 'Playlist de music', icon: Icons.music_note),
 ];
 
@@ -51,16 +56,25 @@ class SelectCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final TextStyle textStyle = Theme.of(context).textTheme.bodyText2;
     return Card(
-        color: Colors.blueGrey,
-
-        child: Center(child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Expanded(child: Icon(choice.icon, size:50.0, color: textStyle.color)),
-              Text(choice.title, style: textStyle),
-            ]
+      color: Colors.deepOrange[300],
+      child: Center(
+        child: Container(
+          padding: EdgeInsets.all(15),
+          height: 200,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                    child:
+                        Icon(choice.icon, size: 50.0, color: textStyle.color)),
+                Text(
+                  choice.title,
+                  style: textStyle,
+                  textAlign: TextAlign.center,
+                ),
+              ]),
         ),
-        )
+      ),
     );
   }
 }
