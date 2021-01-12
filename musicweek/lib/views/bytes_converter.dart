454068f41fd8d480cd94505985b744c9265c6e6e
Riyad <riyad.achour@edu.itesica.fr>
2021-01-12 18:24:28 +0100

    Feature : Added  Dropdownbutton and Textfield

--- musicweek/lib/views/bytes_converter.dart
+++ musicweek/lib/views/bytes_converter.dart
@@ -12,35 +12,90 @@ class _BytesConvertState extends State<BytesConvert> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("test")),
-      body: SizedBox(
-        height: 500,
-        width: 500,
-        child: Card(
-          child: Column(
-            children : [Row(
-                children: <Widget>[
-                  Text("To"),
-                    Expanded(child:
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                        he
+      body: Center(
+        child: SizedBox(
+          height: 500,
+          width: 500,
+          child: Card(
+            child: ListView(
+              padding: EdgeInsets.all(8),
+              children: [
+                Container(
+                  height: 50,
+                  color: Colors.white,
+                  child: Center(
+                      child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Flexible(
+                        child: DropdownButtonFormField<String>(
+                          value: dropdownValue,
+                          style: TextStyle(color: Colors.deepPurple),
+                          onChanged: (String newValue) {
+                            setState(() {
+                              dropdownValue = newValue;
+                            });
+                          },
+                          items: <String>['Octet - O', 'Two', 'Free', 'Four']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      Flexible(
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.person),
+                            labelText: 'Value 1',
+                          ),
+                        ),
                       )
-                    )
-
-                  ),
-                  Text('GO')
-                ]),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Text("hello"),
-                    Text("Hello")
-                  ])
-            ],
-
+                    ],
+                  )),
+                ),
+                Container(
+                  height: 50,
+                  color: Colors.white,
+                  child: Center(
+                      child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Flexible(
+                        child: DropdownButtonFormField<String>(
+                          value: dropdownValue,
+                          style: TextStyle(color: Colors.deepPurple),
+                          onChanged: (String newValue) {
+                            setState(() {
+                              dropdownValue = newValue;
+                            });
+                          },
+                          items: <String>['Octet - O', 'Two', 'Free', 'Four']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      Flexible(
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            icon: Icon(Icons.person),
+                            labelText: 'Value 2',
+                          ),
+                        ),
+                      )
+                    ],
+                  )),
+                ),
+              ],
+            ),
           ),
         ),
       ),
