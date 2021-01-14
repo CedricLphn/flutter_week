429de2c32e3185f0c440ff8ef3cfed2f85261522
Cedric <cedric@nobody.fr>
2021-01-14 13:40:57 +0100

    Add: list view style

--- musicweek/lib/views/Home.dart
+++ musicweek/lib/views/Home.dart
@@ -1,36 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:musicweek/views/stylegrid.dart';
+import 'package:musicweek/views/stylelist.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  String mode = "grid";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[850],
-      appBar: AppBar(
-        leading: Icon(Icons.home),
-        actions: [Icon(Icons.more_vert)],
-        title: Text("Flutter GridView Demo"),
-        backgroundColor: Colors.grey[850],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage("assets/images/background.jpg"),
-              fit: BoxFit.cover),
+      return Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(child:
+              Text(
+                "Utility app",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 23,
+                  color: Colors.white,
+                ),
+              ),
+                decoration: BoxDecoration(
+                    color: Colors.deepOrange[300]
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.grid_on),
+                title: Text("Grid view"),
+                onTap: () {
+                  setState(() {
+                    mode = "grid";
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.list),
+                title: Text("List view"),
+                onTap: () {
+                  setState(() => mode = "list");
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ),
         ),
-        child: GridView.count(
-            crossAxisCount: 3,
-            crossAxisSpacing: 6.0,
-            mainAxisSpacing: 6.0,
-            children: List.generate(choices.length, (index) {
-              return Center(
-                  child: SelectCard(
-                choice: choices[index],
-                route: choices[index].route,
-              ));
-            })),
-      ),
-    );
+        backgroundColor: Colors.grey[850],
+        appBar: AppBar(
+          actions: [Icon(Icons.more_vert)],
+          title: Text("Utility app"),
+          backgroundColor: Colors.grey[850],
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/background.jpg"),
+                fit: BoxFit.cover),
+          ),
+          child: mode == "grid" ? StyleGrid() : StyleView()
+        )
+      );
+
   }
 }
 
@@ -76,39 +114,4 @@ const List<Choice> choices = const <Choice>[
       title: 'Playlist de music', icon: Icons.music_note, route: '/music'),
 ];
 
-class SelectCard extends StatelessWidget {
-  const SelectCard({Key key, this.choice, this.route}) : super(key: key);
-  final Choice choice;
-  final String route;
 
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context).textTheme.bodyText2;
-    return Card(
-      color: Colors.deepOrange[300],
-      child: InkWell(
-        //highlightColor: Colors.red,
-        splashColor: Colors.purpleAccent,
-        radius: 100,
-        onTap: () => choice.changeScreen(context, route),
-
-        child: Container(
-          padding: EdgeInsets.all(15),
-          height: 200,
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                    child:
-                        Icon(choice.icon, size: 50.0, color: textStyle.color)),
-                Text(
-                  choice.title,
-                  style: textStyle,
-                  textAlign: TextAlign.center,
-                ),
-              ]),
-        ),
-      ),
-    );
-  }
-}
