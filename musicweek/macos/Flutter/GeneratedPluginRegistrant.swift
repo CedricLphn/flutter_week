9307ac441fa001fed666f61385441f496fede549
Cedric <cedric@nobody.fr>
2021-01-14 22:54:01 +0100

    Fix: Audio

--- musicweek/macos/Flutter/GeneratedPluginRegistrant.swift
+++ musicweek/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,10 +4,10 @@
 
 import FlutterMacOS
 import Foundation
+
 import audioplayers
 import path_provider_macos
 import shared_preferences_macos
-
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
