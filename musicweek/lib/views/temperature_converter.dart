d339958fb4197e0d11b5b3091676aaf94b10cfd7
Cedric <cedric@nobody.fr>
2021-01-15 11:39:42 +0100

    Fix: algo temperature

--- musicweek/lib/views/temperature_converter.dart
+++ musicweek/lib/views/temperature_converter.dart
@@ -8,15 +8,24 @@ class TempApp extends StatefulWidget {
 class _TempAppState extends State<TempApp> {
   double input;
   double output;
-  bool fOrC;
+  String fOrC;
+  double celsius;
+  double kelvin;
+  double far;
 
   @override
   void initState() {
     super.initState();
     input = 0.0;
     output = 0.0;
-    fOrC = true;
+    fOrC = "F";
   }
+
+  List<String> list = [
+    "F",
+    "C",
+    "K"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +41,7 @@ class _TempAppState extends State<TempApp> {
       },
       decoration: InputDecoration(
         labelText:
-            "Tape une valeur en ${fOrC == false ? "Fahrenheit" : "Celsius"}",
+            "Tape une valeur en ${fOrC == "F" ? "Fahrenheit" : (fOrC == "C") ? "Celsius" : "Kelvin"}",
       ),
     );
 
@@ -51,7 +60,7 @@ class _TempAppState extends State<TempApp> {
           Container(
             margin: const EdgeInsets.all(20),
             child: Text(
-              "F pour Fahrenheit C pour Celsius: ",
+              "F pour Fahrenheit C pour Celsius K pour Kelvin: ",
               style: TextStyle(
                   fontSize: 16,
                   color: Colors.white54,
@@ -59,43 +68,28 @@ class _TempAppState extends State<TempApp> {
             ),
           ),
 
-          // Switch(
-          //   value: fOrC,
-          //   onChanged: (e) {
-          //     setState(() {
-          //       fOrC = !fOrC;
-          //     });
-          //   }
-          // )
-
-          // Checkbox(
-          // value: fOrC,
-          // onChanged: (e) {
-          //   setState(() {
-          //     fOrC = !fOrC;
-          //   });
-          // },
-          //)
-
-          // Radio pour Switcher les degree
-          Text("F"),
-          Radio<bool>(
-              value: false,
-              groupValue: fOrC,
-              onChanged: (v) {
-                setState(() {
-                  fOrC = v;
-                });
-              }),
-          Text("C"),
-          Radio<bool>(
-              value: true,
-              groupValue: fOrC,
-              onChanged: (v) {
-                setState(() {
-                  fOrC = v;
-                });
-              }),
+          // Dropdown pour Switcher les degree
+          DropdownButton<String>(
+            value: fOrC,
+            icon: Icon(Icons.arrow_drop_down),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.brown),
+            underline: Container(
+              height: 2,
+            ),
+            onChanged: (v) {
+              setState(() {
+                fOrC = v;
+              });
+            },
+            items: list.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          )
         ],
       ),
     );
@@ -113,16 +107,19 @@ class _TempAppState extends State<TempApp> {
         child: Text("Valider"),
         onPressed: () {
           setState(() {
-            fOrC == false
-                ? output = (input - 32) * (5 / 9)
-                : output = (input * 9 / 5) + 32;
+            celsius = (fOrC == "K") ? input - 273.15 : (input * 9 / 5) + 32;
+            far =  (fOrC == "K") ? (input - 273.15) * 9/5 + 32: (input - 32) * (5 / 9);
+            kelvin = fOrC == "F" ? (input - 32) * 5/9 + 273.15 : input + 273.15;
+
           });
 
           // Alert pour savoir la temperature
           AlertDialog dialog = AlertDialog(
-            content: fOrC == false
-                ? Text("$input F : $output C")
-                : Text("$input C : $output F"),
+            content: fOrC == "F"
+                ? Text("$input F : $far C : K : $kelvin")
+                : fOrC == "C" ? Text("$input C : $far F : K : $kelvin") :
+            fOrC == "K" ? Text("$input K : $celsius C : F : $far") : ""
+            ,
           );
           showDialog(
             context: context,
